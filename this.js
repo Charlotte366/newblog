928846ec6c3dda0768b8e724c6c3693d933570a3
Charlotte366 <jc.oren@hotmail.com>
2021-06-04 21:20:44 +0200

    update

--- this.js
+++ this.js
@@ -1,65 +1,77 @@
+
+const resultsContainerBlog = document.querySelector(".LatestPosts");
+const arrowRight = document.querySelector(".next")
+const arrowLeft = document.querySelector(".previous")
 let page = 1
-const resultsContainerBlog = document.querySelector(".containerblog");
-const showMoreButton = document.querySelector(".showmore")
 
 
+async function fetchThreePosts() {
 
-async function fetchPosts(){
-
-    try {
-    const url = "https://myblog.charlotte366.no/wp-json/wp/v2/posts?per_page=10&page=" + page + "&_embed=1"
-    console.log(url);
-    const response = await fetch (url);
-    const results = await response.json();
-    const TotalPages = await response.headers.get('X-WP-TotalPages');
-    console.log(results);
-    console.log("Total number of pages:" + TotalPages)
-  
-
-    let html="";
+try {
+const url = "https://myblog.charlotte366.no/wp-json/wp/v2/posts?per_page=3&page=" + page + "&_embed=1"
+const response = await fetch (url)
+const results = await response.json();
+const TotalPages = await response.headers.get('X-WP-TotalPages');
+resultsContainerBlog.innerHTML ="";
+console.log(results);
+console.log("url:" + url)
+console.log("Total number of pages:" + TotalPages)
 
 
-    for (let i = 0; i < results.length; i++) {
-    html =  `<div class="posts">
-    <h3><a href="spesificblogpost.html?id=${results[i].id}"> ${results[i].title.rendered} </h3>
-    <p>Date published:${results[i].date}</p>
-    <div class = "content">
-    <img src= "${results[i]._embedded["wp:featuredmedia"][0].source_url}"  alt= "${results[i]._embedded["wp:featuredmedia"][0].alt_text}">
+let html="";
+
+for (let i = 0; i < results.length; i++) {
+    html +=  `<figure class = "carousel">
+    <a href="spesificblogpost.html?id=${results[i].id}">
+    <img src= "${results[i]._embedded["wp:featuredmedia"][0].source_url}" alt= "${results[i]._embedded["wp:featuredmedia"][0].alt_text}">
     </a>
-    </div>
-    </div>
+    <p>${results[i].title.rendered}</p>
+    </figure>
+    `;
+    resultsContainerBlog.innerHTML = html;
+}
 
-    
-
-                `;
-
-    resultsContainerBlog.innerHTML += html;
-    }
-
-        // More-button visibility
-        //If page number is less than Total Pages, show button
-        if (page >= TotalPages) {
-            showMoreButton.style.display = "none";
+        // Arrows visibility
+        //If page number is more than 1, show previous arrow
+        if (page != 1) {
+            arrowLeft.style.display = "block";
         } else {
-            showMoreButton.style.display = "block";
+            arrowLeft.style.display = "none";
         }
+
+        //If page number is less than Total Pages, show next arrow.
+        if (page >= TotalPages) {
+            arrowRight.style.display = "none";
+        } else {
+            arrowRight.style.display = "block";
+        }
+
 
 }
 
 catch (error) {
     console.log(error);
 }
-
 }
 
-fetchPosts ();
+fetchThreePosts ();
 
 
 
-
-async function showmore() {
+//click right arrow and load three next posts
+async function next() {
     page++
-    await fetchPosts()
+    await fetchThreePosts()
     }
     
-    showMoreButton.addEventListener("click",showmore);
+    arrowRight.addEventListener("click",next);
+
+
+
+//click left arrow and load three previous posts
+async function previous() {
+    page--
+    await fetchThreePosts()
+    }
+    
+    arrowLeft.addEventListener("click",previous);
